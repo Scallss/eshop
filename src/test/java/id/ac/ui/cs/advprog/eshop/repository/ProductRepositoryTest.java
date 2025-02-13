c1962898b2dfb5ec7bd8d0a26f98942431c92bc1
Scallss <pascalhafidz2005@gmail.com>
2025-02-13 15:20:05 +0700

    feat: more edit and delete tests

--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryTest.java
@@ -65,6 +65,66 @@ class ProductRepositoryTest {
     }
 
     @Test
+    void testFindById_ExistingProduct() {
+        Product product = new Product();
+        product.setProductId("123");
+        product.setProductName("Existing Product");
+        product.setProductQuantity(50);
+        productRepository.create(product);
+
+
+        Product foundProduct = productRepository.findById("123");
+
+
+        assertNotNull(foundProduct, "Product should be found");
+        assertEquals("123", foundProduct.getProductId(), "Product ID should match");
+        assertEquals("Existing Product", foundProduct.getProductName(), "Product name should match");
+        assertEquals(50, foundProduct.getProductQuantity(), "Product quantity should match");
+    }
+
+
+    @Test
+    void testFindById_NonExistingProduct() {
+
+        Product foundProduct = productRepository.findById("non-existing-id");
+
+
+        assertNull(foundProduct, "No product should be found with a non-existing ID");
+    }
+
+
+    @Test
+    void testFindById_AmongMultipleProducts() {
+
+        Product product1 = new Product();
+        product1.setProductId("101");
+        product1.setProductName("Product 101");
+        product1.setProductQuantity(10);
+        productRepository.create(product1);
+
+        Product product2 = new Product();
+        product2.setProductId("102");
+        product2.setProductName("Product 102");
+        product2.setProductQuantity(20);
+        productRepository.create(product2);
+
+        Product product3 = new Product();
+        product3.setProductId("103");
+        product3.setProductName("Product 103");
+        product3.setProductQuantity(30);
+        productRepository.create(product3);
+
+
+        Product foundProduct = productRepository.findById("102");
+
+
+        assertNotNull(foundProduct, "Product with ID 102 should be found");
+        assertEquals("102", foundProduct.getProductId(), "Found product ID should match 102");
+        assertEquals("Product 102", foundProduct.getProductName(), "Found product name should match 'Product 102'");
+        assertEquals(20, foundProduct.getProductQuantity(), "Found product quantity should match 20");
+    }
+
+    @Test
     void testEditExistingProduct() {
         Product product = new Product();
         product.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
@@ -116,7 +176,7 @@ class ProductRepositoryTest {
         product.setProductName("Deletable Product");
         productRepository.create(product);
 
-        productRepository.deleteById("999"); // Deleting a non-existing product
+        productRepository.deleteById("999");
 
         Iterator<Product> products = productRepository.findAll();
         assertTrue(products.hasNext(), "Existing products should not be affected by deleting a non-existing one");
